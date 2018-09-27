184dd0f06a717257c04d78cc0cfb274d0e6a05af
joshuageronimo <joshuag31@hotmail.com>
2018-09-26 23:49:49 -0700

    Finish Person class

--- person.py
+++ person.py
@@ -36,14 +36,21 @@ class Person(object):
     def __init__(self, _id, is_vaccinated, infected=None):
         # TODO:  Finish this method.  Follow the instructions in the class documentation
         # to set the corret values for the following attributes.
-        self._id = None
-        self.is_vaccinated = None
-        self.is_alive = None
-        self.infected = None
+        self._id = _id
+        self.is_vaccinated = is_vaccinated
+        self.is_alive = True
+        self.infected = infected
 
 
     def did_survive_infection():
         # TODO:  Finish this method. Follow the instructions in the class documentation
         # for resolve_infection.  If person dies, set is_alive to False and return False.
         # If person lives, set is_vaccinated = True, infected = None, return True.
-        pass
+        mortality_rate = random.randint(0.0,1.0)
+        if mortality_rate > self.infected.mortality_rate:
+            self.is_vaccinated = True
+            self.infected = None
+            return True
+        else
+            self.is_alive = False
+            return False
