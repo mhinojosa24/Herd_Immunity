6344e10e27b9fb7f6762745fd98170c6c6226e24
joshuageronimo <joshuag31@hotmail.com>
2018-10-01 12:34:34 -0700

    Create a Virus class

---
+++ virus.py
@@ -0,0 +1,6 @@
+class Virus(object):
+    def __init__(self, virus_name, mortality_rate, reproductive_rate):
+        self.virus_name = virus_name
+        self.mortality_rate = mortality_rate
+        self.reproductive_rate = reproductive_rate
+    
